eeb2e796a2d4a144116acf934ce472b6ba65de39
Mila <kaah.fernandess@hotmail.com>
2022-03-09 23:37:45 -0300

    Update database config again

--- config/database.js
+++ config/database.js
@@ -4,15 +4,11 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.init('DATABASE_PORT', '127.0.0.1'),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi123'),
-        database: env('DATABASE_SSL', false),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
+        useNullAsDefault: true,
       },
     },
   },
