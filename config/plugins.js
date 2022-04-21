f9e42fc3936f0b1379cecbfbd9e86bea3431890e
Mila <kaah.fernandess@hotmail.com>
2022-04-21 16:54:53 -0300

    plugin graphql

--- config/plugins.js
+++ config/plugins.js
@@ -12,4 +12,15 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+       endpoint: "/graphql",
+       shadowCRUD: true,
+       playgroundAlways: true,
+       depthLimit: 100,
+       apolloServer: {
+         tracing: false,
+         },
+       },
+    },
 });
