95caf626ca6c2128eded163c800aa9c10ef5eb31
Mila <kaah.fernandess@hotmail.com>
2022-04-21 16:46:33 -0300

    Add graphql production

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -10,4 +10,15 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+       endpoint: "/graphql",
+       shadowCRUD: true,
+       playgroundAlways: true,
+       depthLimit: 100,
+       apolloServer: {
+         tracing: false,
+         },
+       },
+    },
 });
